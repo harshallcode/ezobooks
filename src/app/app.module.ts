2ab503e743894fa993671b33dbce3ecbb83350d6
Harshal <harshal480.p@gmail.com>
2023-08-18 20:16:47 +0530

    Added hashLocation for "404 on refresh" issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AtmComponent } from './atm/atm.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,7 @@ import { HomeComponent } from './home/home.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy} ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
